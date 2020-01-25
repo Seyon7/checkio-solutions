5feecbe95713a6fbc9fad1103c3fa17c162e62f5
Seyon7 <moby8481@gmail.com>
2020-01-25 15:58:40 +0200

    some corrections

--- flatten_list.py
+++ flatten_list.py
@@ -7,10 +7,11 @@ def flat_list(array):
             rez = rez + flat_list(i)
     return rez
 
+
 if __name__ == '__main__':
     print(flat_list([1, [2, 2, 2], 4]))
     assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
     assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
-    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([[[2]], [4, [5, 5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 5, 6, 6, 6, 6, 6, 7], "Third"
     assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
     print('Done! Check it')
