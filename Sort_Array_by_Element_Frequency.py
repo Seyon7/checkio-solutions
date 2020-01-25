4ea05266a735f1c26585a321ecd4aeebc245a4db
Seyon7 <moby8481@gmail.com>
2020-01-25 16:40:22 +0200

    Feature1 commit2

--- Sort_Array_by_Element_Frequency.py
+++ Sort_Array_by_Element_Frequency.py
@@ -1,4 +1,4 @@
-def frequency_sort(items: list):
+def frequency_sort(items: list) -> list:
     rez = []
     while True:
         freq = 0
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 
     # These "asserts" are used for self-checking and not for an auto-testing
     assert list(frequency_sort([4, 6, 2, 2, 6, 4, 4, 4])) == [4, 4, 4, 4, 6, 6, 2, 2]
-    assert list(frequency_sort(['bob', 'bob', 'carl', 'alex', 'bob'])) == ['bob', 'bob', 'bob', 'carl', 'alex']
+    assert list(frequency_sort(['bob', 'bob', 'carl', 'alex', 'alex', 'bob'])) == ['bob', 'bob', 'bob', 'alex', 'alex', 'carl']
     assert list(frequency_sort([17, 99, 42])) == [17, 99, 42]
     assert list(frequency_sort([])) == []
     assert list(frequency_sort([1])) == [1]
